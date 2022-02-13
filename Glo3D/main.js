4a9616ba1e45ec5366aba2513278e9970e6c24b9
Yaroslav <tvoibaget@mail.ru>
2022-02-13 17:20:12 +0300

    added deligation

--- Glo3D/main.js
+++ Glo3D/main.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict'
-    const timerHours = document.querySelector('#timer-hours'),
-        timerMinutes = document.querySelector('#timer-minutes'),
-        timerSeconds = document.querySelector('#timer-seconds'),
-        endPromo = new Date(2022, 1, 12, 17, 58);
-    let now = new Date();
 
-    let timeRemaining = endPromo.getTime() - now.getTime()
 
     function promoTimer() {
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds'),
+            endPromo = new Date(2022, 1, 14);
+        let now = new Date();
+
+        let timeRemaining = endPromo.getTime() - now.getTime()
         now = new Date();
         if (timeRemaining > 0) {
             let seconds = (Math.floor(timeRemaining / 1000) % 60)
@@ -41,35 +42,76 @@ document.addEventListener('DOMContentLoaded', () => {
     function toggleMenu() {
         const btnMenu = document.querySelector('.menu'),
             menu = document.querySelector('menu'),
-            btnCloseMenu = document.querySelector('.close-btn'),
-            menuItems = menu.querySelectorAll('ul>li'),
             handlerMenu = () => {
                 menu.classList.toggle('active-menu')
             };
         btnMenu.addEventListener('click', handlerMenu)
-        btnCloseMenu.addEventListener('click', handlerMenu)
-        menuItems.forEach((item) => {
-            item.addEventListener('click', handlerMenu)
+        menu.addEventListener('click', (event) => {
+            let target = event.target;
+            console.log(target.tagName)
+            if (target.tagName !== 'MENU' && target.tagName !== 'LI') {
+                console.log(target)
+                handlerMenu()
+            }
         })
-
     }
     toggleMenu();
 
     function togglePopUp() {
-        let i = 0.1;
+
         const popUp = document.querySelector('.popup'),
-            popUpBtn = document.querySelectorAll('.popup-btn'),
-            popUpCloseBtn = document.querySelector('.popup-close');
+            popUpBtn = document.querySelectorAll('.popup-btn');
 
         popUpBtn.forEach((item) => {
             item.addEventListener('click', () => {
                 popUp.style.display = 'block';
             })
         })
-        popUpCloseBtn.addEventListener('click', () => {
-            popUp.style.display = 'none';
+
+        popUp.addEventListener('click', (event) => {
+            let target = event.target;
+            if (target.classList.contains('popup-close')) {
+                popUp.style.display = 'none';
+            } else {
+                target = target.closest('.popup-content');
+                if (!target) {
+                    popUp.style.display = 'none';
+                }
+            }
         })
 
     };
     togglePopUp();
+
+    function tabs() {
+        const tabHeader = document.querySelector('.service-header'),
+            tab = tabHeader.querySelectorAll('.service-header-tab'),
+            tabContent = document.querySelectorAll('.service-tab');
+        const toggleTabContent = (index) => {
+            for (let i = 0; i < tabContent.length; i++) {
+                if (index === i) {
+                    tab[i].classList.add('active');
+                    tabContent[i].classList.remove('d-none');
+                } else {
+                    tab[i].classList.remove('active');
+                    tabContent[i].classList.add('d-none');
+                }
+            }
+        }
+
+        tabHeader.addEventListener('click', (event) => {
+            let target = event.target;
+            target = target.closest('.service-header-tab')
+            if (target) {
+                tab.forEach((item, i) => {
+                    if (item === target) {
+                        toggleTabContent(i)
+                    }
+                })
+
+            }
+
+        })
+    }
+    tabs();
 })
