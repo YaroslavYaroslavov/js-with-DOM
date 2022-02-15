cd6ec1c77295c4aee70279ddd1ffde5b18c7de66
Yaroslav <tvoibaget@mail.ru>
2022-02-15 19:03:41 +0300

    added slider

--- Glo3D/main.js
+++ Glo3D/main.js
@@ -198,4 +198,26 @@ document.addEventListener('DOMContentLoaded', () => {
         startSlide(10000)
     }
     slider();
+    const changePhoto = () => {
+        const command = document.getElementById('command');
+        let attributeValue;
+
+        command.addEventListener('mouseover', (event) => {
+            const target = event.target;
+            if (!target.matches('.command__photo')) return;
+            else {
+                attributeValue = target.getAttribute('src');
+                target.setAttribute('src', target.getAttribute('data-img'));
+            }
+        });
+        command.addEventListener('mouseout', (event) => {
+            const target = event.target;
+            if (!target.matches('.command__photo')) return;
+            else {
+                target.setAttribute('src', attributeValue);
+            }
+        });
+    };
+    changePhoto()
+
 })
